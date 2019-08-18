dfcdf98603abbcedc9ae56685b06be8e6b2a597d
8e3yn4uk <sashko.kondratenko@gmail.com>
2019-08-18 22:19:07 +0300

    adding exchange

--- src/main/java/guru/springframework/Money.java
+++ src/main/java/guru/springframework/Money.java
@@ -1,6 +1,6 @@
 package guru.springframework;
 
-public class Money {
+public class Money implements Expression {
 
     protected int amount;
     protected String currency;
@@ -24,6 +24,10 @@ public class Money {
 
     public static Money franc(int amount) {
         return new Money(amount, "CHF");
+    }
+
+    public Expression plus(Money addend){
+        return new Money(amount + addend.amount, currency);
     }
 
     public boolean equals(Object object) {
